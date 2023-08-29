e53d60430f6d94bd08ff060c09889ec7b9615a16
Caiyuzhen <caiyuzhen00@gmail.com>
2023-08-30 01:42:19 +0800

    base

---
+++ basic/枚举/enum.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+
+enum WEEK { //枚举量的值从 0 开始依次增加
+	Mon, // = 0
+	Tue, // = 1
+	Wed, // = 3
+	Thu, 
+	Fri,
+	Sat,
+	Sun
+};
+
+
+int main() {
+	WEEK aha = Mon;
+	WEEK kkk = Tue;
+}
