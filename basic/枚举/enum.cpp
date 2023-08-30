76eef6f79a7e0b47e4e8ac3347b5cead111ec99d
ByteDance <caiyuzhen00@gmail.com>
2023-08-30 14:00:12 +0800

    base

--- basic/枚举/enum.cpp
+++ basic/枚举/enum.cpp
@@ -3,18 +3,24 @@
 using namespace std;
 
 
-enum WEEK { //枚举量的值从 0 开始依次增加
+enum WEEK { // 【默认】的枚举量的值从 0 开始依次增加
 	Mon, // = 0
 	Tue, // = 1
 	Wed, // = 3
-	Thu, 
-	Fri,
+	Thu = 10, // 可以直接定义值
+	Fri, // 11
 	Sat,
 	Sun
 };
 
 
 int main() {
-	WEEK aha = Mon;
+	// 赋值定义的方式
+	WEEK aha = Mon; // WEEK 相当于是 aha 的类型
 	WEEK kkk = Tue;
+
+	// 只用强制转化的方式进行赋值
+	WEEK uuu = WEEK(299);
+
+	cout << aha << " " << kkk << " " << uuu << endl;
 }
